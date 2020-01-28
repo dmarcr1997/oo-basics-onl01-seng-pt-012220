d7642b9dc24f1a4bd2831bd964a9361a959720c6
Damon R <dmarcr1997@gmail.com>
2020-01-28 04:30:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,11 @@
 # Make your shoe class here!
 class Shoe
   attr_accessor :size, :material, :condition
+  attr_reader :brand
   def initialize(brand)
     @shoe_brand = brand
   end
+  
+  def cobble
+    
 end
